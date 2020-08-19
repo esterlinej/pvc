ba0f037dadccb04786739b60136fa118a993418f
Alex Janckila <alex.janckila@dollarshaveclub.com>
2020-08-19 15:47:55 -0700

    updates per code review open file - handle not found error ioutil.ReadFile -> ioutil.ReadAll(file)

--- filetree.go
+++ filetree.go
@@ -1,15 +1,15 @@
 package pvc
 
 import (
-	"encoding/base64"
 	"fmt"
-	"os"
 	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
 // Default mapping for this backend
 const (
-	DefaultFileTreeMapping = "{{ .ID }}"
+	DefaultFileTreeMapping  = "{{ .ID }}"
 	DefaultFileTreeRootPath = "/vault/secrets"
 	// TODO make this overrideable through cmd flags
 	MaxFileTreeFileSizeBytes = 2_000_000 // 2 MB```"
@@ -22,7 +22,7 @@ type fileTreeBackendGetter struct {
 }
 
 func newFileTreeBackendGetter(ft *fileTreeBackend) (*fileTreeBackendGetter, error) {
-	// TODO _ check for optional FileTreeRootPath override and handle accordingly 
+	// TODO _ check for optional FileTreeRootPath override and handle accordingly
 	if ft.mapping == "" {
 		ft.mapping = DefaultFileTreeMapping
 	}
@@ -34,34 +34,32 @@ func newFileTreeBackendGetter(ft *fileTreeBackend) (*fileTreeBackendGetter, erro
 		ft.rootPath = DefaultFileTreeRootPath
 	}
 	return &fileTreeBackendGetter{
-		mapper:   sm,
-		config:   ft,
+		mapper: sm,
+		config: ft,
 	}, nil
 }
-
 
 func (ftg *fileTreeBackendGetter) Get(id string) ([]byte, error) {
 	key, err := ftg.mapper.MapSecret(id)
 	if err != nil {
 		return nil, fmt.Errorf("file tree error mapping secret id %v : %v", id, err)
 	}
-	secretFilePath := fmt.Sprintf("%v/%v", ftg.config.rootPath, key)
-	fi, err := os.Stat(secretFilePath)
+	secretFilePath := filepath.Join(ftg.config.rootPath, key)
+	secretFile, err := os.Open(secretFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("file tree error opening file %v: %v", secretFilePath, err)
+	}
+	stat, err := os.Stat(secretFilePath)
 	if err != nil {
 		return nil, fmt.Errorf("file tree error, error getting file stats :%v", err)
 	}
-	size := fi.Size()
+	size := stat.Size()
 	if size > MaxFileTreeFileSizeBytes {
 		return nil, fmt.Errorf("file tree error secret file to large: %v", err)
 	}
-	c, err := ioutil.ReadFile(secretFilePath)
+	c, err := ioutil.ReadAll(secretFile)
 	if err != nil {
-		return nil , fmt.Errorf("file tree error reading file: %v", err)
+		return nil, fmt.Errorf("file tree error reading file: %v", err)
 	}
-	d, err := base64.StdEncoding.DecodeString(string(c))
-	if err != nil {
-		// not base64 encoded
-		return c, nil
-	}
-	return d, nil
+	return c, nil
 }
